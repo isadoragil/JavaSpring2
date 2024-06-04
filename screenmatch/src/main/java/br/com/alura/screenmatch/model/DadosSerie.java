9a248db1ab753975038d529a69c0df098b501a25
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-06-04 11:30:35 -0300

    Initial commit

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias({"Title", "title"}) String titulo,
+                            @JsonAlias("totalSeasons") Integer totalTemporadas,
+                            @JsonAlias("imdbRating") String avaliacao) {
+}
